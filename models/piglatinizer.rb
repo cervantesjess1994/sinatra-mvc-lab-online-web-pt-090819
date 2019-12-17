94d587dab20f60133a176ca806405f1880b95cf7
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-12-17 13:07:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
 
   def piglatinize_word(word)
     first_letter = word[0].downcase
-    
+
     if ["a", "e", "i", "o", "u"].include?(first_letter)
         "#{word}way"
     else
